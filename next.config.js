ec8dc4e7037197ab1ed59b169a1ad6b0a336f097
joejoetommy <joejoetommy@proton.me>
2025-09-04 16:20:57 +0100

    chore: setup netlify adapter

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
+
+// next.config.js
+const { withNetlify } = require('@netlify/next');
+
+module.exports = withNetlify({
+  reactStrictMode: true,
+  // Add other Next.js config options here
+});
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
