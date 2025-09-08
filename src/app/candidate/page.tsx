af96596fc6756ada6af018241b24e62fa924a1cc
joejoetommy <joejoetommy@proton.me>
2025-09-08 21:09:51 +0100

    we done

--- src/app/candidate/page.tsx
+++ src/app/candidate/page.tsx
@@ -10,7 +10,7 @@ const ExplorePage = () => {
   return (
 
                 <div className="flex flex-col pt-4 sm:ml-[120px] md:ml-[250px] sm:border-r sm:border-zinc-700 pb-20 min-h-screen">
-                  <span className="px-8  pb-5 mt-10 font-bold text-3xl">Apply as a Candidate</span>
+                  <span className="px-8  pb-5 mt-10 font-bold text-3xl">Candidate application</span>
                           <Separator/>
                                                 <InformationPacks/>
                         <div className="">
--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -11,7 +11,7 @@ const NotificationsPage = () => {
     return (
 
             <div className="flex flex-col pt-4 sm:ml-[120px] md:ml-[250px] sm:border-r sm:border-zinc-700 pb-20 min-h-screen">
-              <span className="px-8  pb-5 mt-10 font-bold text-3xl">Apply as a Client</span>
+              <span className="px-8  pb-5 mt-10 font-bold text-3xl">Client application</span>
                       <Separator/>
                       <InformationPacks/>
                     <div className="">
